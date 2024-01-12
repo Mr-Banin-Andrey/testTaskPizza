9d3ad78f4a12228d90dfaf2e712d47476c28b810
Andrey Banin <109150204+Mr-Banin-Andrey@users.noreply.github.com>
2024-01-12 16:18:12 +0300

    Initial Commit

---
+++ testTaskPizza/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTaskPizza
+//
+//  Created by Андрей Банин on 12.1.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
